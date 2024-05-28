a12919df0c4b9448c4344d2a1e07bfe33fecdcdb
LaCuci <123963696+LaCuci@users.noreply.github.com>
2024-05-28 22:19:30 +0200

    Gamestate to Json and Json to Gamestate with gooogle.gson.Gson

--- src/main/java/org/example/util/GameStateTranslator.java
+++ src/main/java/org/example/util/GameStateTranslator.java
@@ -1,16 +1,77 @@
 package org.example.util;
 
 import org.example.model.GameState;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GameStateTranslator {
 
-    public static GameState jsonToGameState(String json) {
-        // TODO: Implement the conversion from JSON to GameState
-        return null;
+    private Gson gson;
+
+    public void SaveLoadManager() {
+        this.gson = new Gson();
     }
 
-    public static String gameStateToJson(GameState gameState) {
-        // TODO: Implement the conversion from GameState to JSON
-        return null;
+
+    // STRANA VERSIONE BREVE SENZA TRY CATCH CHE DA ERRORI
+
+    /*
+    public static GameState jsonToGameState(String json) {
+        return gson.fromJson(json, GameState.class);
     }
+    public static String gameStateToJson(GameState gameState) {
+        return gson.toJson(gameState);
+    }
+    */
+
+    //// VERSIONE LUNGA CON TRY CATCH
+
+
+
+    //JSON ----->>>>> GAMESTATE
+
+    public String fileFromPath(String filePath) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+
+            throw new IllegalArgumentException("lettura file fallita");
+        }
+    }
+
+    public GameState loadGameStateFromFile(String filePath) {
+        String json = fileFromPath(filePath);
+        if (json != null) {
+            return recreateGameState(json);
+        } else {
+            throw new IllegalArgumentException("creazione json fallita");
+        }
+    }
+
+    public GameState recreateGameState(String json) {
+        return gson.fromJson(json, GameState.class);
+    }
+
+    //GAMESTATE ----->>>> JSON
+
+    public String saveGameState(GameState gameState) {
+        return gson.toJson(gameState);
+    }
+
+    public void saveGameStateToFile(GameState gameState, String filePath) {
+        String json = saveGameState(gameState);
+        if (json == null) {
+            throw new IllegalArgumentException("creazione json fallita");
+        }
+        try (FileWriter file = new FileWriter(filePath)) {
+            file.write(json);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("salvataggio gamestate fallito");
+        }
+    }
+
+
 }
