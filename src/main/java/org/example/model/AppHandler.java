81e24d1d36b3acbe0e28958da1abb8164e2b1eec
shovelmann <palamin.manuel@gmail.com>
2024-05-28 18:53:17 +0200

    Merge of AppHandler.java and AWSHandler.java structure

--- src/main/java/org/example/model/AppHandler.java
+++ src/main/java/org/example/model/AppHandler.java
@@ -1,4 +1,112 @@
 package org.example.model;
 
-public class AppHandler {
+import org.example.observer.Observable;
+import org.example.observer.Observer;
+import org.example.util.AWSHandler;
+import org.example.util.Event;
+import org.example.util.SaveEvent;
+import org.example.util.UIEvent;
+import org.example.view.handlers.CommandPanelHandler;
+import org.example.view.handlers.GraphicsPanelHandler;
+
+import java.util.ArrayList;
+
+/*
+ * This class aims to handle the entire game loop by coordinating different
+ * game's components.
+ * Observable and Singleton design patterns are implemented.
+ */
+
+public class AppHandler implements Observable {
+
+    private static AppHandler instance = null;
+
+    private ArrayList<Observer> observers = new ArrayList<Observer>();
+    private AppState appState;
+
+    private AppHandler() {
+        appState = AppState.getInstance();
+    }
+
+    public static AppHandler getInstance() {
+        if (instance == null) {
+            instance = new AppHandler();
+        }
+        return instance;
+    }
+
+    @Override
+    public void addObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers(Event e) {
+        if(e instanceof UIEvent) {
+            notifyUI(e);
+        }
+        else if(e instanceof SaveEvent) {
+            notifySave(e);
+        }
+    }
+
+    private void notifySave(Event e) {
+        for (Observer o : observers) {
+            if(o instanceof AWSHandler) {
+                o.update();
+            }
+        }
+    }
+
+    private void notifyUI(Event e) {
+        for (Observer o : observers) {
+            if(o instanceof CommandPanelHandler || o instanceof GraphicsPanelHandler) {
+                o.update();
+            }
+        }
+    }
+
+    public void startGame() {
+        // TODO
+    }
+
+    public void exitToMenu() {
+        //TODO
+    }
+
+    public void move(int locationID) {
+        //TODO
+    }
+
+    public void pickUpItem(int entityID) {
+        //TODO
+    }
+
+    public void useItem(int entityID) {
+        //TODO
+    }
+
+    public void dropItem(int entityID) {
+        //TODO
+    }
+
+    public String getHelp() {
+        //TODO
+        return null;
+    }
+
+    public String getDialogue(int entityID) {
+        //TODO
+        return null;
+    }
+
+    public String getDescription(int entityID) {
+        //TODO
+        return null;
+    }
 }
--- src/main/java/org/example/util/AWSHandler.java
+++ src/main/java/org/example/util/AWSHandler.java
@@ -1,4 +1,36 @@
 package org.example.util;
 
-public class AWSHandler {
+import org.example.observer.Observable;
+import org.example.observer.Observer;
+
+public class AWSHandler implements Observer {
+
+    private static AWSHandler instance = null;
+
+    private AWSHandler() {}
+
+    public static AWSHandler getInstance() {
+        if (instance == null) {
+            instance = new AWSHandler();
+        }
+        return instance;
+    }
+
+    @Override
+    public void update() {
+        // TODO
+    }
+
+    public void saveToS3(String data) {
+        // TODO
+    }
+
+    public String loadFromS3() {
+        // TODO
+        return null;
+    }
+
+    public void jsonToObject() {
+        // TODO
+    }
 }
