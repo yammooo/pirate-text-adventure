6434d48695023317d21a803ae7fe1af1338071ef
Gianmaria Frigo <gianmariafrigo3@gmail.com>
2024-05-28 20:34:44 +0200

    skeleton of GameStateTranslator

---
+++ src/main/java/org/example/util/GameStateTranslator.java
@@ -0,0 +1,16 @@
+package org.example.util;
+
+import org.example.model.GameState;
+
+public class GameStateTranslator {
+
+    public static GameState jsonToGameState(String json) {
+        // TODO: Implement the conversion from JSON to GameState
+        return null;
+    }
+
+    public static String gameStateToJson(GameState gameState) {
+        // TODO: Implement the conversion from GameState to JSON
+        return null;
+    }
+}
