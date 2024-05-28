aa826babec8c1097f60d107abf423fddb39edc03
Gianmaria Frigo <gianmariafrigo3@gmail.com>
2024-05-28 18:26:45 +0200

    added AppState.java and GameState.java implementation

--- src/main/java/org/example/model/AppState.java
+++ src/main/java/org/example/model/AppState.java
@@ -1,4 +1,41 @@
 package org.example.model;
 
+import org.example.model.entities.enums.WindowState;
+
 public class AppState {
+    private static AppState instance;
+    private GameState gameState;
+    private WindowState currentWindow;
+
+    private AppState() {
+        gameState = null;
+        currentWindow = WindowState.MENU;
+    }
+
+    public static AppState getInstance() {
+        if (instance == null) {
+            instance = new AppState();
+        }
+        return instance;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public WindowState getCurrentWindow() {
+        return currentWindow;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    public void setWindowToMenu() {
+        currentWindow = WindowState.MENU;
+    }
+
+    public void setWindowToGame() {
+        currentWindow = WindowState.GAME;
+    }
 }
--- src/main/java/org/example/model/GameState.java
+++ src/main/java/org/example/model/GameState.java
@@ -1,4 +1,22 @@
 package org.example.model;
 
+import org.example.model.entities.Pirate;
+import org.example.model.entities.Map;
+
 public class GameState {
+    private final Pirate pirate;
+    private final Map map;
+
+    private GameState(Pirate pirate, Map map) {
+        this.pirate = pirate;
+        this.map = map;
+    }
+
+    public Pirate getPirate() {
+        return pirate;
+    }
+
+    public Map getMap() {
+        return map;
+    }
 }
