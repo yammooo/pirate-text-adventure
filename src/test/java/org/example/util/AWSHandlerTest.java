9f420620a92f3e2c3f6c0d1ed9b2de61f43ba0a4
matteobravin <bravinmatteo@gmail.com>
2024-06-06 17:00:00 +0200

    Added new test classes

---
+++ src/test/java/org/example/util/AWSHandlerTest.java
@@ -0,0 +1,40 @@
+package org.example.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AWSHandlerTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void countSavedGames() {
+    }
+
+    @Test
+    void loadFromS3() {
+    }
+
+    @Test
+    void getGamesTitles() {
+    }
+
+    @Test
+    void getSavedGames() {
+    }
+
+    @Test
+    void saveAnExistingGame() {
+    }
+
+    @Test
+    void deleteGame() {
+    }
+}
---
+++ src/test/java/org/example/view/MainFrameTest.java
@@ -0,0 +1,16 @@
+package org.example.view;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainFrameTest {
+
+    @Test
+    void getCommandPanel() {
+    }
+
+    @Test
+    void getGraphicsPanel() {
+    }
+}
