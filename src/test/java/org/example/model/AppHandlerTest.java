e1dceac769053eaa08add85082920ae043606e41
shovelmann <palamin.manuel@gmail.com>
2024-06-07 01:09:47 +0200

    Modified AppHandlerTest.java

--- src/test/java/org/example/model/AppHandlerTest.java
+++ src/test/java/org/example/model/AppHandlerTest.java
@@ -2,119 +2,241 @@ package org.example.model;
 
 import org.example.exceptions.AWSException;
 import org.example.exceptions.BackpackWeightExceededException;
+import org.example.exceptions.ItemNotFoundException;
 import org.example.model.entities.*;
 import org.example.model.entities.enums.WindowState;
+import org.example.pair.Pair;
 import org.example.util.AWSHandler;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
+/*
+ * in order to succeed in all tests of this class, both an internet access and resources/AWS/credentials.properties file are required
+ */
 class AppHandlerTest {
 
-    private AppHandler appHandler;
-    private AppState appState;
-    private AWSHandler awsHandler;
-
-    @BeforeEach
-    void setUp() {
-        appHandler = AppHandler.getInstance();
-        appState = AppState.getInstance();
-        awsHandler = mock(AWSHandler.class);
-        appHandler.addObserver(awsHandler);
-    }
-
     @Test
-    void getInstanceReturnsSingletonInstance() {
+    void singletonTest() {
         AppHandler instance1 = AppHandler.getInstance();
         AppHandler instance2 = AppHandler.getInstance();
         assertSame(instance1, instance2);
     }
 
     @Test
-    void getAppStateReturnsCorrectAppState() {
-        assertEquals(appState, appHandler.getAppState());
+    void startNewGameTest() {
+        AppHandler.getInstance().startNewGame();
+        assertEquals(WindowState.GAME, AppHandler.getInstance().getAppState().getCurrentWindow());  // check if the state of the game has turned into GAME
     }
 
     @Test
-    void startNewGameChangesWindowToGame() {
-        appHandler.startNewGame();
-        assertEquals(WindowState.GAME, appState.getCurrentWindow());
+    void startSavedGameTest() {
+        AppHandler.getInstance().startSavedGame(1);
+        assertEquals(WindowState.GAME, AppHandler.getInstance().getAppState().getCurrentWindow());  // check if the state of the game has turned into GAME
     }
 
     @Test
-    void startSavedGameChangesWindowToGame() {
+    void startSavedGameFailureTest() {
         try {
-            when(awsHandler.getSavedGames(anyInt())).thenReturn("{}");
-            appHandler.startSavedGame(1);
-            assertEquals(WindowState.GAME, appState.getCurrentWindow());
+            AppHandler.getInstance().startSavedGame(AWSHandler.getInstance().countSavedGames() + 1);
+            assertEquals(WindowState.MENU, AppHandler.getInstance().getAppState().getCurrentWindow());  // check if the state of the game remains set to MENU
         } catch (AWSException e) {
-            fail("AWSException should not have been thrown.");
+            fail("AWSException should not have been thrown here."); // this exception would be caught if countSaveGames() trigger it
         }
     }
 
     @Test
-    void startSavedGameFailsGracefullyWhenAWSExceptionOccurs() {
-        try {
-            when(awsHandler.getSavedGames(anyInt())).thenThrow(new AWSException("AWS error"));
-            appHandler.startSavedGame(1);
-            assertEquals(WindowState.MENU, appState.getCurrentWindow());
-        } catch (AWSException e) {
-            // Expected exception
-        }
+    void exitToMenuTest() {
+        AppHandler.getInstance().exitToMenu();
+        assertEquals(WindowState.MENU, AppHandler.getInstance().getAppState().getCurrentWindow());  // check if the state of the game has turned into MENU
     }
 
     @Test
-    void exitToMenuChangesWindowToMenu() {
-        appHandler.exitToMenu();
-        assertEquals(WindowState.MENU, appState.getCurrentWindow());
-    }
+    void moveWithNoObstacleTest() {
+        Pirate pirate = new Pirate(new Backpack(10), 3);
 
-    @Test
-    void moveChangesLocationWhenNoObstacle() {
-        Pirate pirate = new Pirate(new Backpack(50), 3);
-
-        ArrayList<Integer> adjecentLocations = new ArrayList<>();
-        adjecentLocations.add(2);
-        adjecentLocations.add(1);
+        ArrayList<Integer> adjacentLocations1 = new ArrayList<>();
+        adjacentLocations1.add(2);
 
         ArrayList<Location> locations = new ArrayList<>();
-        Location location1 = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjecentLocations);
-        Location location2 = new Location(2, "Location2", "Description2", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjecentLocations);
+        Location location1 = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjacentLocations1);
+        Location location2 = new Location(2, "Location2", "Description2", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        locations.add(location1);
+        locations.add(location2);
+
+        // no obstacles in the path from location 1 to location 2
+
+        Map map = new Map(locations, new HashMap<>(), 1);
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().move(2);
+        assertEquals(2, AppHandler.getInstance().getAppState().getGameState().getMap().getPirateLocationID());  // check if current location of pirate has changed
+    }
+
+    @Test
+    void moveLosingFightAgainstObstacleTest() {
+        Pirate pirate = new Pirate(new Backpack(10), 3);
+
+        // no items in the backpack, let alone the item to defeat the obstacle
+
+        ArrayList<Integer> adjacentLocations1 = new ArrayList<>();
+        adjacentLocations1.add(2);
+
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location1 = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjacentLocations1);
+        Location location2 = new Location(2, "Location2", "Description2", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        locations.add(location1);
+        locations.add(location2);
+
+        HashMap<Pair<Integer, Integer>, Obstacle> obstacles = new HashMap<>();
+        Obstacle obstacle = new Obstacle(1, "Obstacle1", "Description1", 1);
+        obstacles.put(new Pair<>(1, 2), obstacle);
+
+        Map map = new Map(locations, obstacles, 1);
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().move(2);
+        assertEquals(1, AppHandler.getInstance().getAppState().getGameState().getMap().getPirateLocationID());  // check if pirate remains in the same location
+    }
+
+    @Test
+    void moveWinningFightAgainstObstacleTest() {
+        Backpack backpack = new Backpack(10);
+        CollectableItem itemToDefeatObstacle = new CollectableItem(1, "Item1", "Description1", 1, 0);
+        try {
+            backpack.addItem(itemToDefeatObstacle); // item needed for defeat obstacle equipped
+        } catch (BackpackWeightExceededException e) {
+            fail("AWSException should not have been thrown here.");
+        }
+
+        Pirate pirate = new Pirate(backpack, 3);
+
+        ArrayList<Integer> adjacentLocations1 = new ArrayList<>();
+        adjacentLocations1.add(2);
+
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location1 = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjacentLocations1);
+        Location location2 = new Location(2, "Location2", "Description2", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        locations.add(location1);
+        locations.add(location2);
+
+        HashMap<Pair<Integer, Integer>, Obstacle> obstacles = new HashMap<>();
+        Obstacle obstacle = new Obstacle(1, "Obstacle1", "Description1", 1);
+        obstacles.put(new Pair<>(1, 2), obstacle);
+
+        Map map = new Map(locations, obstacles, 1);
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().move(2);
+        assertEquals(2, AppHandler.getInstance().getAppState().getGameState().getMap().getPirateLocationID());  // check if locations has changed
+    }
+
+    @Test
+    void gameOverTest() {
+        Pirate pirate = new Pirate(new Backpack(10), 1);    // 1 life only and no items in the backpack
+
+        ArrayList<Integer> adjacentLocations1 = new ArrayList<>();
+        adjacentLocations1.add(2);
+
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location1 = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjacentLocations1);
+        Location location2 = new Location(2, "Location2", "Description2", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        locations.add(location1);
+        locations.add(location2);
+
+        HashMap<Pair<Integer, Integer>, Obstacle> obstacles = new HashMap<>();
+        Obstacle obstacle = new Obstacle(1, "Obstacle1", "Description1", 1);
+        obstacles.put(new Pair<>(1, 2), obstacle);
+
+        Map map = new Map(locations, obstacles, 1);
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().move(2);
+        assertEquals(WindowState.GAME_OVER, AppHandler.getInstance().getAppState().getCurrentWindow()); // check if the state of the game has turned into GAME_OVER
+    }
+
+    @Test
+    void gameWinTest() {
+        Backpack backpack = new Backpack(10);
+        CollectableItem itemToDefeatObstacle = new CollectableItem(4, "Legendary Key", "legendary key description", 1, 0);
+        try {
+            backpack.addItem(itemToDefeatObstacle); // item needed for pass through the portal that bring to the treasure island
+        } catch (BackpackWeightExceededException e) {
+            fail("AWSException should not have been thrown here.");
+        }
+
+        Pirate pirate = new Pirate(backpack, 3);
+
+        ArrayList<Integer> adjacentLocations1 = new ArrayList<>();
+        adjacentLocations1.add(0);  // 0 is the id of the treasure island location
+
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location1 = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), adjacentLocations1);
+        Location location2 = new Location(0, "Treasure Island", "treasure island description", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
         locations.add(location1);
         locations.add(location2);
 
         Map map = new Map(locations, new HashMap<>(), 1);
-        appState.setGameState(new GameState(pirate, map, "Title1"));
-        appHandler.move(2);
-        assertEquals(2, appState.getGameState().getMap().getPirateLocationID());
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().move(0);
+        assertEquals(WindowState.GAME_WIN, AppHandler.getInstance().getAppState().getCurrentWindow()); // check if the state of the game has turned into GAME_WIN
     }
 
+
     @Test
-    void pickUpItemAddsItemToBackpack() {
-        Pirate pirate = new Pirate(new Backpack(50), 3);
-        Map map = new Map(new ArrayList<>(), new HashMap<>(), 1);
-        appState.setGameState(new GameState(pirate, map, "Title1"));
-        appHandler.pickUpItem(1);
+    void pickUpItemWithNoRequiredItemTest() {
+        Pirate pirate = new Pirate(new Backpack(10), 3);
+
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        location.addCollectableItem(new CollectableItem(1, "Item1", "Description1", 1, 0)); // item to collect with no required item
+        locations.add(location);
+
+        Map map = new Map(locations, new HashMap<>(), 1);
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().pickUpItem(1);
         assertTrue(pirate.getBackpack().isItemEquipped(1));
     }
 
     @Test
-    void dropItemRemovesItemFromBackpack() {
+    void pickUpItemWithRequiredItemTest() {
+        Pirate pirate = new Pirate(new Backpack(10), 3);
+
+        ArrayList<Location> locations = new ArrayList<>();
+        Location location = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        location.addCollectableItem(new CollectableItem(1, "Item1", "Description1", 1, 2)); // item to collect with required item
+        locations.add(location);
+
+        Map map = new Map(locations, new HashMap<>(), 1);
+        AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+        AppHandler.getInstance().pickUpItem(1);
+        assertFalse(pirate.getBackpack().isItemEquipped(1));
+    }
+
+    @Test
+    void dropItemTest() {
         try {
-            Pirate pirate = new Pirate(new Backpack(50), 3);
-            Map map = new Map(new ArrayList<>(), new HashMap<>(), 1);
-            appState.setGameState(new GameState(pirate, map, "Title1"));
-            pirate.getBackpack().addItem(new CollectableItem(1, "Item1", "Description1", 1, 0));
-            appHandler.dropItem(1);
-            assertFalse(pirate.getBackpack().isItemEquipped(1));
-        } catch (BackpackWeightExceededException e) {
+            Pirate pirate = new Pirate(new Backpack(10), 3);
+            CollectableItem itemToDrop = new CollectableItem(1, "Item1", "Description1", 1, 0); // item to drop
+            pirate.getBackpack().addItem(itemToDrop);
+
+            ArrayList<Location> locations = new ArrayList<>();
+            Location location = new Location(1, "Location1", "Description1", new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+            locations.add(location);
+
+            Map map = new Map(locations, new HashMap<>(), 1);
+            AppHandler.getInstance().getAppState().setGameState(new GameState(pirate, map, "Title1"));
+
+            AppHandler.getInstance().dropItem(1);
+            // check both whether the item is not in the backpack anymore and if it has added in the current island
+            assertTrue(!AppHandler.getInstance().getAppState().getGameState().getPirate().getBackpack().isItemEquipped(1) && location.getCollectableItemByID(1) == itemToDrop);
+        } catch (BackpackWeightExceededException | ItemNotFoundException e) {
             fail("BackpackWeightExceededException should not have been thrown.");
         }
     }
